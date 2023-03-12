53993ed56c084f86065e028280f3757951d1d320
SlavaIPZ <127666313+SlavaIPZ@users.noreply.github.com>
2023-03-12 23:12:18 +0200

    Updated reader. It can now set its information source to a file.

--- src/Main.java
+++ src/Main.java
@@ -12,37 +12,61 @@ import java.util.List;
                     System.out.println("1)Add faculty.\n2)Edit faculty\n3)Delete faculty.");
                     int mode =Reader.readInt(1,3);
                     switch (mode){
-                        case 1:
-                        case 2:
-                        case 3:
+                        case 1:System.out.println("Enter the name of faculty:");
+                        String facultyName=Reader.readLine();
+                        break;
+                        case 2:System.out.println("Choose the faculty to edit:");
+
+
+                        case 3:System.out.println("Choose the faculty to delete:");
                     }
                     break;
                 case 2:
                     System.out.println("1)Add department.\n2)Edit department\n3)Delete department.");
                     mode =Reader.readInt(1,3);
                     switch (mode){
-                        case 1:
-                        case 2:
-                        case 3:
+                        case 1:System.out.println("Enter the name of department:");
+                        case 2:System.out.println("Choose the department to edit:");
+                        case 3:System.out.println("Choose the department to delete:");
                     }
+                    break;
                 case 3:
-                    System.out.println("1)Add student.\n2)Edit student\n3)Delete student.\nSort students.");
-                    mode =Reader.readInt(1,3);
+                    System.out.println("1)Add student.\n2)Edit student\n3)Delete student.\n4)Get students.\n5)Find student.");
+                    mode =Reader.readInt(1,5);
                     switch (mode){
-                        case 1:
-                        case 2:
-                        case 3:
-                        case 4:
+                        case 1:System.out.println("Enter the name of student:");
+                            System.out.println("Enter the age of student:");
+                            System.out.println("Enter the department of student:");
+                            System.out.println("Enter the group of student:");
+                            System.out.println("Enter the year of study of student:");
+                            break;
+                        case 2:System.out.println("Choose the student to edit:");
+                        case 3:System.out.println("Choose the student to delete:");
+                        case 4:System.out.println("Choose the way of getting list:\n1)By alphabetical order.\n2)By year of study.");
+                        int way=Reader.readInt(1,2);
+                        switch (way){
+                            case 1:
+                            case 2:
+                        }
+                        case 5:System.out.println("Enter the name of student:");
                     }
+                    break;
                 case 4:
-                    System.out.println("1)Add teacher.\n2)Edit teacher\n3)Delete teacher.\nSort teachers.");
-                    mode =Reader.readInt(1,3);
+                    System.out.println("1)Add teacher.\n2)Edit teacher\n3)Delete teacher.\n4)Get list of teachers by alphabetical order.\n5)Find teacher.");
+                    mode =Reader.readInt(1,4);
+                    mode =Reader.readInt(1,5);
                     switch (mode){
-                        case 1:
-                        case 2:
-                        case 3:
+                        case 1:System.out.println("Enter the name of teacher:");
+                            System.out.println("Enter the age of teacher:");
+                            System.out.println("Enter the department of teacher:");
+                            break;
+                        case 2:System.out.println("Choose the teacher to edit:");
+                        case 3:System.out.println("Choose the teacher to delete:");
                         case 4:
+                        case 5:System.out.println("Enter the name of teacher:");
                     }
+                    break;
+
 
 
 
