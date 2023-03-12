3d5e6b4ef04319cc61a2181fbb519812d1f559d8
SlavaIPZ <127666313+SlavaIPZ@users.noreply.github.com>
2023-03-12 22:46:01 +0200

    Updated reader. It can now set its information source to a file.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,55 @@
-public class Main {
-    private static University NaUKMA;
-    public static void main(String[] args) {
-//        NaUKMA = new University();
+
+    import java.util.ArrayList;
+import java.util.List;
+
+    public class Main {
+        public static void main(String[] args) {
+            System.out.println("MENU\n1)Edit faculty.\n2)Edit department.");
+            System.out.println("\n3)Edit students.\n4)Edit teachers.");
+            int option =Reader.readInt(1,4);
+            switch (option){
+                case 1 :
+                    System.out.println("1)Add faculty.\n2)Edit faculty\n3)Delete faculty.");
+                    int mode =Reader.readInt(1,3);
+                    switch (mode){
+                        case 1:
+                        case 2:
+                        case 3:
+                    }
+                    break;
+                case 2:
+                    System.out.println("1)Add department.\n2)Edit department\n3)Delete department.");
+                    mode =Reader.readInt(1,3);
+                    switch (mode){
+                        case 1:
+                        case 2:
+                        case 3:
+                    }
+                case 3:
+                    System.out.println("1)Add student.\n2)Edit student\n3)Delete student.\nSort students.");
+                    mode =Reader.readInt(1,3);
+                    switch (mode){
+                        case 1:
+                        case 2:
+                        case 3:
+                        case 4:
+                    }
+                case 4:
+                    System.out.println("1)Add teacher.\n2)Edit teacher\n3)Delete teacher.\nSort teachers.");
+                    mode =Reader.readInt(1,3);
+                    switch (mode){
+                        case 1:
+                        case 2:
+                        case 3:
+                        case 4:
+                    }
+
+
+
+
+
     }
 
-    /**
-     * This method creates a menu with options
-     */
-    public static void menu(){
-
     }
+
 }
