ebaf7de0f3e562a63291506bb1964a00f06d007f
W1nLin4n <grechkaartema@gmail.com>
2023-03-16 18:46:32 +0200

    Changed access level of faculties ArrayList

--- src/University.java
+++ src/University.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class University {
-    public List<Faculty> faculties;
+    private List<Faculty> faculties;
 
     public University(File input){
         faculties = new ArrayList<>();
